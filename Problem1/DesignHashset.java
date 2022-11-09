3221c701ce24452fb7770e9f4c2d698708bf1c0b
super30admin <52191330+super30admin@users.noreply.github.com>
2022-11-10 00:35:43 +0530

    Design 2

---
+++ Problem1/DesignHashset.java
@@ -0,0 +1,60 @@
+package Problem1;
+// Time Complexity : O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
+
+public class DesignHashset {
+    static class MyHashSet {
+        private boolean[][] arr;
+        private int len;
+        public MyHashSet() {
+            len = 1000;
+            arr = new boolean[len][];
+        }
+        private int hash1(int key) {
+            return key % 1000;
+        }
+        private int hash2(int key) {
+            return key / 1000;
+        }
+        public void add(int key) {
+            int index = hash1(key);
+            if(arr[index] == null) {
+                if(index == 0) {
+                    arr[index] = new boolean[len +1];
+                } else {
+                    arr[index] = new boolean[len];
+                }
+            }
+            arr[index][hash2(key)] = true;
+        }
+
+        public void remove(int key) {
+            int index = hash1(key);
+            if(arr[index] == null) {
+                return;
+            }
+            arr[index][hash2(key)] = false;
+
+        }
+
+        public boolean contains(int key) {
+            int index = hash1(key);
+            if(arr[index] == null) {
+                return false;
+            }
+            return arr[index][hash2(key)];
+        }
+
+        public static void main(String[] args) {
+            MyHashSet set = new MyHashSet();
+            set.add(2);
+            set.add(3);
+            set.add((4));
+            System.out.println("does hash has 3 " + set.contains(3));
+            set.remove(3);
+            System.out.println("does hash has 3 " + set.contains(3));
+        }
+    }
+}
---
+++ Problem2/DesignMinStack.java
@@ -0,0 +1,51 @@
+package Problem2;
+// Time Complexity : O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
+
+import java.util.*;
+public class DesignMinStack {
+    static class MinStack {
+        Stack<Integer> stk;
+        Stack<Integer> minStk;
+        public MinStack() {
+            stk = new Stack<>();
+            minStk = new Stack<>();
+        }
+
+        public void push(int val) {
+            stk.push(val);
+
+            if(minStk.isEmpty() || val <= minStk.peek()) {
+                minStk.push(val);
+            }
+        }
+
+        public void pop() {
+            int val = stk.pop();
+            if(!minStk.isEmpty() && minStk.peek() == val) {
+                minStk.pop();
+            }
+        }
+
+        public int top() {
+            return stk.peek();
+        }
+
+        public int getMin() {
+            return minStk.peek();
+        }
+    }
+
+    public static void main(String[] args) {
+        MinStack stk = new MinStack();
+        stk.push(2);
+        stk.push(3);
+        stk.push(1);
+        stk.push(-1);
+        System.out.println("Minimum number in satck: " + stk.getMin());
+        stk.pop();
+        System.out.println("Minimum number in satck: " + stk.getMin());
+    }
+}
